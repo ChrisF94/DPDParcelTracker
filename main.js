9273c6681dd113d82fed1c561c2b8508227784b0
ChrisF94 <48619715+ChrisF94@users.noreply.github.com>
2022-04-03 00:23:21 +0100

    add a delay event

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    post: '3306',
     user: 'root',
     password: '',
     database: 'parcelTracker'
@@ -22,27 +23,29 @@ function getTrackingStatus(parcelNumber) {
 
             if (theData.includes('delivered')) {
                 var delivered = 'Yes';
-            }else{
+            } else {
                 var delivered = 'No';
             }
-            
+
             connection.query('UPDATE parcels SET parcelStatus = ?, delivered = ? WHERE parcelNumber = ?', [theData, delivered, parcelNumber], (err, result) => {
                 if (err) throw err;
             });
-        })
-        .catch(error => {
+        }).catch(error => {
             console.log(error);
         });
 }
 
-function getTrackingNumbers(){
-    connection.query('SELECT parcelNumber FROM parcels WHERE delivered = ""', (err, rows) => {
+function getTrackingNumbers() {
+    connection.query('SELECT parcelNumber FROM parcels WHERE delivered = "No"', (err, rows) => {
         if (err) throw err;
         for (let index = 0; index < rows.length; index++) {
             const parcelNumber = rows[index].parcelNumber;
-            getTrackingStatus(parcelNumber);
+            setTimeout(() => {
+                console.log(parcelNumber);
+                getTrackingStatus(parcelNumber);
+            }, index * 1000);
         }
-    });     
+    });
     return "DPD API call complete";
 }
 
